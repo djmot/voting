7699393e306aa20715b72678fbfa450651012029
David <djmzorn@gmail.com>
2016-10-06 22:51:44 +0000

    initial from clementine.js boilerplate, configured for c9

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect('mongodb://'+process.env.IP+'/clementinejs');
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -27,7 +27,6 @@ app.use(passport.session());
 
 routes(app, passport);
 
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.listen(process.env.PORT, process.env.IP, function () {
+	console.log('Node.js listening on port ' + process.env.PORT);
 });
